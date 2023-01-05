3e9aff5a1a5f99de82df466ff13c337a076b2b37
Mousumi Mou <ridimarahmanmou5804@gmail.com>
2023-01-05 13:21:04 +0600

    bug fixed

--- src/redux/reducers/productReducer.js
+++ src/redux/reducers/productReducer.js
@@ -29,6 +29,18 @@ console.log(selectedProduct);
                 cart:[...state.cart, {...action.payload, quantity: 1}]
             };
         case REMOVE_FROM_CART:
+            if(selectedProduct.quantity > 1){
+                const newCart = state.cart.filter(
+                    (product)=> product._id !== selectedProduct._id
+                );
+                selectedProduct.quantity = selectedProduct.quantity - 1;
+
+                return {
+                    ...state,
+                    cart: [...newCart, selectedProduct],
+                }
+            }
+
             return {
                 ...state,
                 cart: state.cart.filter(
